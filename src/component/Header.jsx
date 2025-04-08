25f51b1d6d64bd4cf350ed2449ebfa9684ecab12
kang <rkdgusdn0319@gmail.com>
2025-04-08 14:21:43 +0900

    commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -94,114 +94,120 @@ export default function Header() {
 
       {/* 헤더 영역 */}
       <div className="absolute z-30 w-full bg-transparent">
-        <div className="relative flex items-center justify-between px-32 h-66">
-          <div className="flex gap-20">
-            <button
-              type="button"
-              onClick={() => location.pathname !== "/login" && setShowSeries((prev) => !prev)}
-            >
-              <FontAwesomeIcon
-                className={`w-24 h-24 ${showSeries
+        <div className={`relative flex items-center ${(location.pathname !== '/login') ? 'justify-between' : 'justify-center'} px-32 h-66`}>
+          {
+            (location.pathname !== '/login') &&
+            <div className="flex gap-20">
+              <button
+                type="button"
+                onClick={() => location.pathname !== "/login" && setShowSeries((prev) => !prev)}
+              >
+                <FontAwesomeIcon
+                  className={`w-24 h-24 ${showSeries
                     ? "text-black"
                     : iconColor === "white"
                       ? "text-white"
                       : "text-black"
-                  }`}
-                icon={faBars}
-              />
-            </button>
-            {/* 검색 아이콘 클릭 시 모달 오픈 */}
-            <button
-              type="button"
-              onClick={() => setIsSearchOpen(true)}
-            >
-              <FontAwesomeIcon
-                className={`w-24 h-24 ${showSeries
+                    }`}
+                  icon={faBars}
+                />
+              </button>
+              {/* 검색 아이콘 클릭 시 모달 오픈 */}
+              <button
+                type="button"
+                onClick={() => setIsSearchOpen(true)}
+              >
+                <FontAwesomeIcon
+                  className={`w-24 h-24 ${showSeries
                     ? "text-black"
                     : iconColor === "white"
                       ? "text-white"
                       : "text-black"
-                  }`}
-                icon={faMagnifyingGlass}
-              />
-            </button>
-          </div>
+                    }`}
+                  icon={faMagnifyingGlass}
+                />
+              </button>
+            </div>
+          }
           <Link to="/" className="h-40 w-120">
             <img
               src="/images/casetibuy-logo.png"
               alt="Casetify 로고"
             />
           </Link>
-          <div className="flex gap-20">
-            <div
-              className="relative"
-              ref={dropdownRef}
-              onMouseLeave={() => setIsDropdownOpen(false)}
-            >
-              <button
-                type="button"
-                {...(isLoggedIn
-                  ? { onMouseOver: toggleDropdown }
-                  : { onClick: toggleDropdown })}
+          {
+            (location.pathname !== '/login') &&
+            <div className="flex gap-20">
+              <div
+                className="relative"
+                ref={dropdownRef}
+                onMouseLeave={() => setIsDropdownOpen(false)}
               >
-                <FontAwesomeIcon
-                  className={`w-24 h-24 py-15 ${showSeries
+                <button
+                  type="button"
+                  {...(isLoggedIn
+                    ? { onMouseOver: toggleDropdown }
+                    : { onClick: toggleDropdown })}
+                >
+                  <FontAwesomeIcon
+                    className={`w-24 h-24 py-15 ${showSeries
                       ? "text-black"
                       : iconColor === "white"
                         ? "text-white"
                         : "text-black"
-                    }`}
-                  icon={faUser}
-                />
-              </button>
-              {isDropdownOpen && (
-                <ul className="absolute right-0 mt-2 text-black bg-white shadow-2xl top-50 rounded-15 w-120">
-                  {isLoggedIn && location.pathname !== "/mypage" && (
-                    <li>
-                      <Link
-                        to="/mypage"
-                        className="block px-4 text-center py-14 hover:text-grayph text-14"
-                        onClick={() => setIsDropdownOpen(false)}
-                      >
-                        마이페이지
-                      </Link>
-                    </li>
-                  )}
-                  {isLoggedIn && (
-                    <li>
-                      <button
-                        onClick={() => {
-                          handleLogout();
-                          setIsDropdownOpen(false);
-                        }}
-                        className="block w-full px-4 text-center py-14 hover:text-grayph text-14"
-                      >
-                        로그아웃
-                      </button>
-                    </li>
-                  )}
-                </ul>
-              )}
-            </div>
-            <div className="flex gap-2">
-              <button type="button" onClick={toggleCart} className="relative">
-                <FontAwesomeIcon
-                  className={`w-24 h-24 ${showSeries
+                      }`}
+                    icon={faUser}
+                  />
+                </button>
+                {isDropdownOpen && (
+                  <ul className="absolute right-0 mt-2 text-black bg-white shadow-2xl top-50 rounded-15 w-120">
+                    {isLoggedIn && location.pathname !== "/mypage" && (
+                      <li>
+                        <Link
+                          to="/mypage"
+                          className="block px-4 text-center py-14 hover:text-grayph text-14"
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
+                          마이페이지
+                        </Link>
+                      </li>
+                    )}
+                    {isLoggedIn && (
+                      <li>
+                        <button
+                          onClick={() => {
+                            handleLogout();
+                            setIsDropdownOpen(false);
+                          }}
+                          className="block w-full px-4 text-center py-14 hover:text-grayph text-14"
+                        >
+                          로그아웃
+                        </button>
+                      </li>
+                    )}
+                  </ul>
+                )}
+              </div>
+              <div className="flex gap-2">
+                <button type="button" onClick={toggleCart} className="relative">
+                  <FontAwesomeIcon
+                    className={`w-24 h-24 ${showSeries
                       ? "text-black"
                       : iconColor === "white"
                         ? "text-white"
                         : "text-black"
-                    }`}
-                  icon={faCartShopping}
-                />
-              </button>
-              {cartCount > 0 && (
-                <div className="relative w-16 h-16 pt-2 text-center text-white bg-black rounded-full top-4 text-12">
-                  {cartCount}
-                </div>
-              )}
+                      }`}
+                    icon={faCartShopping}
+                  />
+                </button>
+                {cartCount > 0 && (
+                  <div className="relative w-16 h-16 pt-2 text-center text-white bg-black rounded-full top-4 text-12">
+                    {cartCount}
+                  </div>
+                )}
+              </div>
             </div>
-          </div>
+          }
         </div>
       </div>
       {location.pathname !== "/products" && (
